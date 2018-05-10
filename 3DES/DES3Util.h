9374b132a814e30f2d55130909aaff9d28d01166
leechance <workinterface@126.com>
2018-05-10 15:55:53 +0800

    3DES加密
    3DES加密

---
+++ 3DES/DES3Util.h
@@ -0,0 +1,19 @@
+//
+//  DES3Util.h
+//  lx100-gz
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface DES3Util : NSObject {
+
+}
+
+// 加密方法
++ (NSString*)encrypt:(NSString*)plainText;
+
+// 解密方法
++ (NSString*)decrypt:(NSString*)encryptText;
+
+@end
